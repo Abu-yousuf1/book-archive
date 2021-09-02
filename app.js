0f9f4b64afd068b2f25589e692d971007b712682
abu-yousuf1 <yousufadnan111@gmail.com>
2021-09-02 18:26:53 +0600

    almost complite

--- app.js
+++ app.js
@@ -23,20 +23,19 @@ const displayBooks = (books) => {
 
         const div = document.createElement('div')
         div.classList.add('col')
-        const { cover_i, title, author_name, publisher, first_publish_year } = book
-        if (author_name == undefined || publisher == undefined) {
-            console.log(" null", book)
-            return
-        }
-
+        const emptyImage = "https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg"
+        const image = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+        const author = book.author_name ? book?.author_name[0] : "not found"
+        const published = book.publisher ? book?.publisher : "not found"
+        const coverImage = book?.cover_i ? image : emptyImage
         div.innerHTML = `
         <div class="card">
-        <img src="https://covers.openlibrary.org/b/id/${cover_i}-M.jpg" class="img-fluid" alt="...">
+        <img src="${coverImage}" class="img-fluid" alt="...">
         <div class="card-body">
-            <h5 class="text-center">Book:<span >${title}</span></h5>
-             <h6 class="tex-center">Author: <span> ${author_name[0]}</span></h6>
-             <h6 class="text-center">Publisher: <span> ${publisher[0]}</span> </h6>
-             <h6 class= "text-center">publish Date: <span> ${first_publish_year}</span> </h6>
+            <h5 class="text-center">Book:<span >${book.title}</span></h5>
+             <h6 class="tex-center">Author: <span> ${author}</span></h6>
+             <h6 class="text-center">Publisher: <span> ${published}</span> </h6>
+             <h6 class= "text-center">publish Date: <span> ${book.first_publish_year}</span> </h6>
         </div>
     </div>
         `
