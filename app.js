c068ac5e1ec54c93f2a0d94c8451c8bd32cf7221
abu-yousuf1 <yousufadnan111@gmail.com>
2021-09-02 20:38:43 +0600

    complite

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+// get Api data ............................
 const getData = async () => {
     const searchField = document.getElementById('search-field')
     const searchValue = searchField.value;
     displaySpinner('block')
     toggleSearchResult('none')
+    copyRight('none')
     totalBook('none')
     const url = `https://openlibrary.org/search.json?q=${searchValue}`
     const res = await fetch(url);
@@ -12,41 +14,54 @@ const getData = async () => {
     searchField.value = '';
 }
 
+// display books...............................................
+
 const displayBooks = (books) => {
     const cardContainer = document.getElementById('card-container')
+    const errBookName = document.getElementById('err-book-name')
     cardContainer.textContent = '';
     displaySpinner('none')
     toggleSearchResult('block')
     totalBook('block')
-    books.forEach(book => {
-        console.log(book)
+    copyRight('block')
+    if (books.length === 0) {
+        errBookName.style.display = "block"
+        copyRight('none')
+        totalBook('none')
+    } else {
+        errBookName.style.display = "none"
+        books.forEach(book => {
+            console.log(book)
 
-        const div = document.createElement('div')
-        div.classList.add('col')
-        const emptyImage = "https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg"
-        const image = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
-        const author = book.author_name ? book?.author_name[0] : "not found"
-        const published = book.publisher ? book?.publisher : "not found"
-        const coverImage = book?.cover_i ? image : emptyImage
-        div.innerHTML = `
+            const div = document.createElement('div')
+            div.classList.add('col')
+            const emptyImage = "https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg"
+            const image = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+            const author = book?.author_name ? book.author_name[0] : "Not Found"
+            const published = book?.publisher ? book.publisher : "Not Found"
+            const coverImage = book?.cover_i ? image : emptyImage
+            div.innerHTML = `
         <div class="card">
-        <img src="${coverImage}" class="img-fluid" alt="...">
+        <img src="${coverImage}" class="card-img-top img-fluid"  style="width: 300px; height: 200px;" alt="...">
         <div class="card-body">
-            <h5 class="text-center">Book:<span >${book.title}</span></h5>
-             <h6 class="tex-center">Author: <span> ${author}</span></h6>
-             <h6 class="text-center">Publisher: <span> ${published}</span> </h6>
-             <h6 class= "text-center">publish Date: <span> ${book.first_publish_year}</span> </h6>
+            <h5 class="text-primary">Book:<span >${book.title.slice(0, 15)}</span></h5>
+             <h6 class="text-danger">Author: <span> ${author}</span></h6>
+             <h6 class="">Publisher: <span> ${published}</span> </h6>
+             <h6 class= "">publish Date: <span> ${book.first_publish_year}</span> </h6>
         </div>
     </div>
         `
-        cardContainer.appendChild(div)
+            cardContainer.appendChild(div)
 
-    })
-    const size = Object.keys(books).length;
-    const cardLength = document.getElementById('length-card')
-    cardLength.innerText = size
-    console.log(size)
+        })
+        const size = Object.keys(books).length;
+        const cardLength = document.getElementById('length-card')
+        cardLength.innerText = size
+        console.log(size)
+    }
 }
+
+// search result ,spinner and total book toggling.....................................
 
 const displaySpinner = (display) => {
     const spinner = document.getElementById('spinner')
@@ -57,4 +72,7 @@ const toggleSearchResult = (display) => {
 }
 const totalBook = (display) => {
     document.getElementById('total-book').style.display = display
+}
+const copyRight = display => {
+    document.getElementById('copy-right').style.display = display
 }
